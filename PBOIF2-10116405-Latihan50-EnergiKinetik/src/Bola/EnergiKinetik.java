35712521eb2a1f3a642114ffa473c77e70819f7c
Fahrih Fatahilah <painchikusudo@gmail.com>
2020-11-15 03:10:15 +0800

    Add files via upload

---
+++ PBOIF2-10116405-Latihan50-EnergiKinetik/src/Bola/EnergiKinetik.java
@@ -0,0 +1,14 @@
+package Bola;
+/**
+ * @author Fahrih Fatahilah
+ * Kelas : IF-2 / PBO-IF2
+ * Nim   : 10116405
+ * Deskripsi Program : Menghitung Energi Kinetik dan Usaha dalam suatu bola
+**/
+public class EnergiKinetik {
+    public static void main(String[] args) {       
+        BolaBaseball bola1 = new BolaBaseball(0.145, 25);
+        bola1.tampil();
+    }
+    
+}
